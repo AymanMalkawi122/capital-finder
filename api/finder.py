55063e71cd5debd0ac0165150fed158589a8f1a4
AymanMalkawi122 <malkawiayman63@gmail.com>
2023-06-13 00:35:01 +0300

    16

--- api/finder.py
+++ api/finder.py
@@ -10,11 +10,11 @@ class handler(BaseHTTPRequestHandler):
     self.send_header('Content-type', 'text/plain')
     self.end_headers()
     url_components = parse.urlsplit(self.path)
-    req_type = url_components.query.split()[0]
+    req_type = url_components.query.split("=")[0]
     if req_type == "country":
         req_type = "name"
     else: req_type = "capital"
-    query_params = url_components.query.split()[1]
+    query_params = url_components.query.split("=")[1]
     req = requests.get(f"https://restcountries.com/v3.1/{req_type}/" + query_params)
     message = str(req.capital if req_type == "capital" else req.name.comon)
     self.wfile.write(message.encode())
