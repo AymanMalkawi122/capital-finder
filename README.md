8f5c86ba9bff6e49862f1f16c21b617a28ff57d0
AymanMalkawi122 <malkawiayman63@gmail.com>
2023-06-12 23:32:50 +0300

    16

---
+++ README.md
@@ -0,0 +1,2 @@
+https://capital-finder-vuhk-7ge2k05zx-aymanmalkawi122.vercel.app/api/finder?capital=amman
+https://capital-finder-vuhk-7ge2k05zx-aymanmalkawi122.vercel.app/api/finder?country=jordan
--- api/finder.py
+++ api/finder.py
@@ -10,17 +10,13 @@ class handler(BaseHTTPRequestHandler):
     self.send_response(200)
     self.send_header('Content-type', 'text/plain')
     self.end_headers()
-    self.wfile.write(str(datetime.now().strftime('%Y-%m-%d %H:%M:%S')).encode())
-    # self.send_response(200)
-    # self.send_header('Content-type', 'text/plain')
-    # self.end_headers()
-    # url_components = parse.urlsplit(self.path)
-    # req_type = url_components.query.split()[0]
-    # if req_type == "country":
-    #     req_type = "name"
-    # query_params = url_components.query.split()[1]
-    # req = requests.get(f"https://restcountries.com/v3.1/{req_type}/" + query_params)
-    # self.wfile.write(str(req.capital if req_type == "capital" else req.name.comon).encode())
+    url_components = parse.urlsplit(self.path)
+    req_type = url_components.query.split()[0]
+    if req_type == "country":
+        req_type = "name"
+    query_params = url_components.query.split()[1]
+    req = requests.get(f"https://restcountries.com/v3.1/{req_type}/" + query_params)
+    self.wfile.write(str(req.capital if req_type == "capital" else req.name.comon).encode())
     return
 
 
