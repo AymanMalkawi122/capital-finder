44dc48da016a6a7525f431cbec819a8933958614
AymanMalkawi122 <malkawiayman63@gmail.com>
2023-06-12 22:43:05 +0300

    16

---
+++ api/capital_finder.py
@@ -0,0 +1,19 @@
+from http.server import BaseHTTPRequestHandler
+import requests
+from urllib import parse
+
+
+class handler(BaseHTTPRequestHandler):
+ 
+  def do_GET(self):
+    self.send_response(200)
+    self.send_header('Content-type', 'text/plain')
+    self.end_headers()
+    url_components = parse.urlsplit(self.path)
+    req_type = url_components.query.split()[0]
+    if req_type == "country":
+        req_type = "name"
+    query_params = url_components.query.split()[1]
+    req = requests.get(f"https://restcountries.com/v3.1/{req_type}/" + query_params)
+    self.wfile.write(str(req.capital if req_type == "capital" else req.name.comon).encode())
+    return
